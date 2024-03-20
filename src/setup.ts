f5b15419932fe78dd5f4f78f91136b53bdf7a68c
L0uzzDev <l0uzzdev@gmail.com>
2024-03-20 12:33:16 -0300

    first commit

---
+++ src/setup.ts
@@ -0,0 +1,15 @@
+import { sql } from "./lib/postgres";
+
+async function setup(){
+  await sql/*sql*/`
+  CREATE TABLE IF NOT EXISTS shortlinks(
+    id SERIAL PRIMARY KEY,
+    code TEXT UNIQUE,
+    original_url TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  )
+  `
+  await sql.end()
+
+  console.log("Setup feito com sucesso"); 
+}
